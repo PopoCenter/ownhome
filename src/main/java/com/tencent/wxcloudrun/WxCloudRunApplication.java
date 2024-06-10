9e4687750a264c64ec733c641e1938d596c1215f
xiedongdong <xiedd@ehualu.com>
2024-06-10 22:23:25 +0800

    fix bug

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
 public class WxCloudRunApplication {  
